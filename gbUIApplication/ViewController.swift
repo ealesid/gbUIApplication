35f827a1e0c8a0968a17e80adfd5d4bdf8cbadaa
Aleksey Sidorov <ealesid@mymini.local>
2019-03-02 14:32:37 +0300

    Initial Commit

---
+++ gbUIApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gbUIApplication
+//
+//  Created by Aleksey Sidorov on 02/03/2019.
+//  Copyright © 2019 Aleksey Sidorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
